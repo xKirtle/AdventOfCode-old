8398688c43d07c14ec749496abb9422e20d031c3
Kirtle <xkirtle26@gmail.com>
2021-12-03 04:21:31 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+// handleInput(1);
+
+static string[] handleInput(int day)
+{
+    string[] input;
+    string path = Path.Combine(Environment.CurrentDirectory, $"Input\\Day{day}.txt");
+
+    using (StreamReader streamReader = new StreamReader(path, Encoding.UTF8))
+        input = streamReader.ReadToEnd().Split(Environment.NewLine);
+
+    return input;
+}
